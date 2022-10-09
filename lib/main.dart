c5f5c6761d3f129379ab6d8b0513cdb24be5e7b0
Vasudev Nagar <112718032+dev-247@users.noreply.github.com>
2022-10-09 22:01:25 +0530

    update from github

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
 //========================================================//
 //                 SETTING FILTER    and Favorites        //
+//      some changes from github
 //========================================================//
   Map<String, bool> _filters = {
     'gluten': false,
